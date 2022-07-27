168787f68405394de92287e9f70b8c1b80728f3c
Mark Shepherd <markshep1@hotmail.com>
2022-07-27 19:41:10 +0100

    Alter class name in html and CSS for '=' button. Create event listener to compute product when '=' pressed

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ let numberArray = [];
 let operator_val;
 let funcArray = [];
 let numString;
+let product = 0;
 
 function add(a ,b) {
     return a + b;
@@ -55,16 +56,35 @@ const operators = document.querySelectorAll('.operator');
 let opArray = Array.from(operators);
 opArray.forEach(button => {
    button.addEventListener('click', () => {
-       if(funcArray.length == 3) {
-            let product = operate(funcArray[0], funcArray[1], funcArray[2]);
+        funcArray.push(numString);
+        if(funcArray.length == 3) {
+            product = operate(funcArray[0], funcArray[1], funcArray[2]);
             let display = document.querySelector('.text');
-            display.textContent = product;
-       }
-       let display = document.querySelector('.text');
-       operator_val = button.id;
-       display.textContent = operator_val;
-       funcArray.push(numString);
-       funcArray.push(operator_val);
-       numberArray = [];
+            funcArray = [];
+            funcArray.push(product);
+            funcArray.push(operator_val);
+            display.textContent = operator_val;
+            numberArray = [];
+        }
+        
+        else {
+            let display = document.querySelector('.text');
+            operator_val = button.id;
+            display.textContent = operator_val;
+            funcArray.push(operator_val);
+            numberArray = [];
+        }
+        });
    });
+
+const equals = document.querySelector('.evaluate');
+equals.addEventListener('click', () => {
+    funcArray.push(numString);
+    if(funcArray.length == 3) {
+        product = operate(funcArray[0], funcArray[1], funcArray[2]);
+        let display = document.querySelector('.text');
+        display.textContent = product;
+        funcArray = [];
+        funcArray.push(product);
+    }
 })
